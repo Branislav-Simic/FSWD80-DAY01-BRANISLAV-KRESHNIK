9a3aee6cd5803b04b07902f2fc154b53d5f91bdf
Branislav Simic <branislavsmc@outlook.de>
2019-09-19 14:59:01 +0200

    Challanges done : replace and diagonal in js

---
+++ challanges/challange.js
@@ -0,0 +1,8 @@
+var city=[
+["Vienna","0","0","0","0"],
+["Vienna","is","a","nice","city"],
+["Vienna","is","a","nice","city"],
+["Vienna","is","a","nice","city"],
+["Vienna","is","a","nice","city"]
+];
+console.log(city[0][0] +' ' +city[1][1] +' '+ city[2][2] +' '+ city[3][3] +' '+ city[4][4]);
