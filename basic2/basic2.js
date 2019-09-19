9794b675dd9811384d967c4a0c7687f50094cf3e
Branislav Simic <branislavsmc@outlook.de>
2019-09-19 10:43:22 +0200

    basic4 done: cosole with array

--- basic2/basic2.js
+++ basic2/basic2.js
@@ -1,2 +1,14 @@
 var age = 25;
 console.log("Hi my name is Martin and i am "+age+" years old");
+
+
+ players=['Martin','Thomas','Peter','Mathias','Niki'];
+console.log (players [3]);
+console.log (players [4]);
+console.log("The most valuable of the match is "+players [3]+' and '+players[4]);
+
+/*var player1=(player[3]);
+var player2=(player[4]);
+var team=player1+''+player2;
+
+console.log()*/
