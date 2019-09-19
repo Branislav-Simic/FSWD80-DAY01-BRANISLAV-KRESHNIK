63ff839543ccfed3dd8a181d11659a4a6477ef33
kreshnik <kreshnikzeka4@gmail.com>
2019-09-19 13:52:42 +0200

    advanced done: array calculate and selcet and output

---
+++ advanced1to3/advanced1.js
@@ -0,0 +1,16 @@
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+var x = a+b+Number(c)+Number(d)+e;
+console.log(x);
+
+var f ="1";
+var g = 15;
+var h = 8;
+var i = "1";
+var y = Number(f)*g*h*Number(i);
+console.log(y);
+
+document.write(x/y);
